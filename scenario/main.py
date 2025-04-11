0fae8f18319b9cfca35ac9a0a9247d1b5d6c304d
root-hbx <huboxuan2004@gmail.com>
2025-04-11 13:34:46 +0000

    (udp-test) add statistic

--- scenario/main.py
+++ scenario/main.py
@@ -204,6 +204,19 @@ def run_scenario():
     logging.info("Theoretical Time: 70 seconds")
     logging.info(f"Results saved to {output_file}")
     logging.info("Scenario completed successfully")
+    
+    total_time = elapsed_sysclose
+    t_autoclose_actualclose_udp1 = stage1_time - (35 + built1_probe - scenario_start)
+    t_autoclose_actualclose_udp2 = stage2_time - (35 + built2_probe - stage2_probe)
+    t_roaming_cost = total_time - 35 - 35 - t_autoclose_actualclose_udp1 - t_autoclose_actualclose_udp2
+
+    with open(output_file, "a") as f:
+        f.write("\n")
+        f.write("statistic: \n")
+        f.write(f"total_time = {total_time:.4f}\n")
+        f.write(f"t_autoclose_actualclose_udp1 = {(t_autoclose_actualclose_udp1):.4f}\n")
+        f.write(f"t_autoclose_actualclose_udp2 = {(t_autoclose_actualclose_udp2):.4f}\n")
+        f.write(f"t_roaming_cost = {t_roaming_cost:.4f}\n")
 
 
 if __name__ == "__main__":
@@ -214,13 +227,13 @@ if __name__ == "__main__":
     run_scenario()
     time.sleep(3)
     
-    # # Different bandwidth
-    # for i in range(10, 210, 10):
-    #     bw = f"{i}M"
-    #     try:
-    #         BW4UDP = bw
-    #         run_scenario()
-    #     except Exception as e:
-    #         logging.error(f"Error during test with bandwidth {bw}: {e}")
-    #     finally:
-    #         time.sleep(3)
+    # Different bandwidth
+    for i in range(10, 210, 10):
+        bw = f"{i}M"
+        try:
+            BW4UDP = bw
+            run_scenario()
+        except Exception as e:
+            logging.error(f"Error during test with bandwidth {bw}: {e}")
+        finally:
+            time.sleep(3)
